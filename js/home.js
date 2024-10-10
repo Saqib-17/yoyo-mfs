bb0c65af3bdb4222316eae5fc3f0cda99af66c79
Saqib-17 <sakib17shahidul@gmail.com>
2024-10-11 01:04:41 +0600

    Add money feature completed!

--- js/home.js
+++ js/home.js
@@ -4,11 +4,31 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
     
     //get added money
     const addedMoney = document.getElementById('input-add-money').value;
-    console.log(addedMoney);
+    
     //get added pin
     const pinNum = document.getElementById('input-pin-num').value;
-    console.log(pinNum);
+    
 
-    
-    
+    //match the pin
+    if(pinNum==='1234'){
+        console.log("Adding money to your account");
+
+        //get the current balance
+        const prevMoney = document.getElementById('account-money').innerText;
+
+        //update the balance
+
+        const addedBalance = parseFloat(addedMoney);
+        const addedPrevBalance = parseFloat(prevMoney);
+        const updateBalance = addedBalance + addedPrevBalance;
+        console.log(updateBalance);
+
+        //add the updated balance to original balance
+        document.getElementById('account-money').innerText=updateBalance;
+
+    }
+    else
+    {
+        alert("Failed to add money! Please try again ");
+    }
 });
